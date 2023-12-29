6a95dc5f7d9173919254efe2e1add9bbfccd3fe8
Alejandro Corredor <acorredor@stoalabs.com>
2023-12-29 17:47:10 -0500

    initial scene

---
+++ ClickToMove.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class ClickToMove : CharacterBody3D
+{
+  public const float Speed = 5.0f;
+  public const float JumpVelocity = 4.5f;
+
+  // Get the gravity from the project settings to be synced with RigidBody nodes.
+  public float gravity = ProjectSettings
+    .GetSetting("physics/3d/default_gravity")
+    .AsSingle();
+
+  public override void _PhysicsProcess(double delta)
+  {
+    return;
+  }
+}
